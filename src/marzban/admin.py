bef629ffec4c5491df2499443eebe21196f8ebc3
kirill <kirisa49@gmail.com>
2025-08-19 16:05:39 +0700

    fulled admin.py

--- src/marzban/admin.py
+++ src/marzban/admin.py
@@ -4,6 +4,7 @@ class AdminFunctions:
     def __init__(self, panel: Panel):
         self.panel = panel
         self.admin_prefix = "admin"
+        self.admins_prefix = "admins"
 
     async def get_token(self, username: str, password: str):
         data = {
@@ -51,4 +52,71 @@ class AdminFunctions:
             data=data
         )
         
+        return response
+
+    async def modify_admin(
+        self,
+        username: str,
+        password: str = None,
+        is_sudo: bool = None,
+        telegram_id: int = None,
+        discord_webhook: str = None
+    ):
+        data = {k: v for k, v in {
+            "password": password,
+            "is_sudo": is_sudo,
+            "telegram_id": telegram_id,
+            "discord_webhook": discord_webhook
+        }.items() if v is not None}
+
+        response = await self.panel.request(
+            endpoint=f"{self.admin_prefix}/{username}",
+            method="PUT",
+            data=data
+        )
+        return response
+
+    async def delete_admin(
+        self,
+        username: str
+    ):
+        response = await self.panel.request(
+            endpoint=f"{self.admin_prefix}/{username}",
+            method="DELETE"
+        )
+        return response
+
+    async def get_admins(self):
+        response = await self.panel.request(
+            endpoint=f"{self.admins_prefix}",
+            method="GET"
+        )
+        return response
+
+    async def disable_all_active_users(self, username: str):
+        response = await self.panel.request(
+            endpoint=f"{self.admin_prefix}/{username}/users/disable",
+            method="POST"
+        )
+        return response
+
+    async def activate_all_disabled_users(self, username: str):
+        response = await self.panel.request(
+            endpoint=f"{self.admin_prefix}/{username}/users/activate",
+            method="POST"
+        )
+        return response
+
+    async def reset_admin_usage(self, username: str):
+        response = await self.panel.request(
+            endpoint=f"{self.admin_prefix}/usage/reset/{username}",
+            method="POST"
+        )
+        return response
+
+    async def get_admin_usage(self, username: str):
+        response = await self.panel.request(
+            endpoint=f"{self.admin_prefix}/usage/{username}",
+            method="GET"
+        )
         return response
